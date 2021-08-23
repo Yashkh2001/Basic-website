90ea3bf9f5c6036565848b6df8e3da202d714a21
Yashkh2001 <yash.kh2001@gmail.com>
2021-08-23 17:20:48 +0530

    third commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer=()=>{
  return(
   <footer className ="bg-light text-center w-100">
-      <p>@ 2020, All rights reserved</p>
+      <p>@ 2020, All rights</p>
   </footer>
 
  );
